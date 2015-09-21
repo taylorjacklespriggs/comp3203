e1330c94b221bc0a8b46ca315fb59ec945d358d0
taylor <_blank_@live.ca>
2015-09-21 12:02:20 -0400

    FIRST COMMIT!

---
+++ Assignment1/calls.py
@@ -0,0 +1,23 @@
+
+from subprocess import *
+
+def ls(directory):
+    '''Calls ls -l $directory'''
+    return check_output(('ls', '-l', directory))
+
+def cd(directory):
+    '''Checks if $directory is valid and returns absolute path'''
+    return check_output(('readlink', '-f', directory))
+
+def put(filename, content):
+    '''Puts $content into $filename'''
+    f = open(filename, 'w+')
+    f.write(content)
+    f.close()
+
+def get(filename):
+    '''Returns the contents of $filename'''
+    f = open(filename, 'r')
+    content = f.read()
+    f.close()
+
