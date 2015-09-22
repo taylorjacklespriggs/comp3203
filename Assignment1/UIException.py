8376d7c44b2857c0d175aab3ce6404f4bc9fa6b5
Paul Dunsworth <pauldunsworth@cmail.carleton.ca>
2015-09-22 13:49:42 -0400

    Added file for UI Exceptions.

---
+++ Assignment1/UIException.py
@@ -0,0 +1,10 @@
+class DirectoryError(Exception):
+    pass
+class PutServerError(Exception):
+    pass
+class PutClientError(Exception):
+    pass
+class GetServerError(Exception):
+    pass
+class GetClientError(Exception):
+    pass
