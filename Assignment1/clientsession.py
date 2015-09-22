7c0c5f14f44cd9647436374b76e39716a5049750
Eric <eric-therrien@hotmail.com>
2015-09-22 15:27:46 -0400

    removed my slashes

--- Assignment1/clientsession.py
+++ Assignment1/clientsession.py
@@ -1,13 +1,13 @@
 class ClientSession(object):
-	def __init__(self, directory):
-		self.home_directory = directory
+        def __init__(self, directory):
+                self.home_directory = directory
                 self.current_directory = directory
-		
-	def __str__(self):
-		return "Current directory is %s" % (self.directory)
-	
-	def change_directory(self, newDirectory):
-		self.current_directory = newDirectory
+                
+        def __str__(self):
+                return "Current directory is %s" % (self.directory)
+        
+        def change_directory(self, newDirectory):
+                self.current_directory = newDirectory
 
         def reset_directory(self):
                 self.current_directory = self.home_directory
