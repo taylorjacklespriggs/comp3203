c9509cbea5b1ccec998bb6eba601f29698c9c39d
Charron, Christopher <charch84@gmail.com>
2015-09-22 16:01:45 +0000

    Finished the first version of the clientui.py.

--- Assignment1/clientui.py
+++ Assignment1/clientui.py
@@ -24,6 +24,8 @@ class ClientUI:
             self.cd(command[1])
         if command[0] == "put":
             self.put(command[1], command[2])
+        if command[0] == "get":
+            self.put(command[1], command[2])
         if command[0] == "q":
             return 1;
 
@@ -33,7 +35,8 @@ class ClientUI:
                 "q -  stops the client\n\t" +
                 "ls <directory> - lists everything in <directory>\n\t" +
                 "cd <directory> - changes the current directory to <directory>\n\t" +
-                "put <src> <dest> - put the src file from the client to the dest file on the server\n\t")
+                "put <src> <dest> - put the src file from the client to the dest file on the server\n\t" +
+                "get <src> <dest> - get the src file from the server to the destination file on the client\n\t")
 
     def ls(self, directory):
         ''' asks the controller to return a list of everything in
@@ -58,10 +61,22 @@ class ClientUI:
             controller for transferring a file from the client to the server '''
         try:
             self.controller.put(srcfile, destfile)
+            print("transfer successful!")
         except PutServerError:
             print("Could not write to server...")
         except PutClientError:
             print("Could not find file on client...")
+
+    def get(self, srcfile, destfile):
+        ''' sends the source filename and destination filename to the controller
+            for transferring a file from the server to the client '''
+        try:
+            self.controller.get(srcfile, destfile)
+            print("transfer successful!")
+        except GetServerError:
+            print("Could not find file on server...")
+        except GetClientError:
+            print("Could not write file to client...")
 
 if __name__ == "__main__":
     client = ClientUI()
