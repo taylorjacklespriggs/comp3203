df111ed1d74ff4bb554cae4d4e61eaec59adc3d1
Charron, Christopher <charch84@gmail.com>
2015-09-29 14:18:02 +0000

    Reverted changes to the main.py

---
+++ Assignment1/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import controller
+
+def main():
+    print("Test for controller")
+    contrl = controller.Controller()
+    contrl.start()
+
+if __name__ == "__main__":
+    main()
